0095383510cac7cf990f567aa816422b0f9d9c3b
saqibshehzad424 <shehzadsaqib424@gmail.com>
2022-04-26 10:16:19 +0500

    Add project files.

---
+++ POINT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POINT.BL;
+namespace POINT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject gameObject = new GameObject();
+         
+            while (true)
+            {
+                Thread.Sleep(100);
+                gameObject.Erase();
+                gameObject.Move();
+                gameObject.Draw();
+            }
+        }
+    }
+}
---
+++ POINT/UI/Ui.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POINT.BL;
+namespace POINT.UI
+{
+    class Ui
+    {
+        public static void draw_shap(char[,] shape,point boundary)
+        {
+            int m = 0;int n = 0;
+            for(int i=boundary.getX(); i < (boundary.getX()+ shape.GetLength(0)); i++)
+            {
+                n = 0;
+                for(int j=boundary.getY(); j< (boundary.getY() + shape.GetLength(1)); j++)
+                {
+                    Console.SetCursorPosition(j, i);
+                    Console.Write(shape[m,n]);
+                    n++;
+                }
+                m++;
+                Console.WriteLine();
+            }
+        }
+        public static void erase_shape(char[,] shape, point boundary)
+        {
+            int m = 0; int n = 0;
+            for (int i = boundary.getX(); i < (boundary.getX() + shape.GetLength(0)); i++)
+            {
+                n = 0;
+                for (int j = boundary.getY(); j < (boundary.getY() + shape.GetLength(1)); j++)
+                {
+                    Console.SetCursorPosition(j, i);
+                    Console.Write(" ");
+                    n++;
+                }
+                m++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
